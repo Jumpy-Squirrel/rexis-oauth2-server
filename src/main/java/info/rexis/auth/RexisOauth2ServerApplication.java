071adaf127664dc37b18675a6f61ca6aa4bfef2c
Jumpy-Squirrel <jsquirrel_github_9a6d@packetloss.de>
2018-12-31 11:21:48 +0100

    initial commit of spring 2.1 app from initializr, upgraded to gradle 5.0

---
+++ src/main/java/info/rexis/auth/RexisOauth2ServerApplication.java
@@ -0,0 +1,11 @@
+package info.rexis.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RexisOauth2ServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RexisOauth2ServerApplication.class, args);
+	}
+}
---
+++ src/test/java/info/rexis/auth/RexisOauth2ServerApplicationTests.java
@@ -0,0 +1,14 @@
+package info.rexis.auth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RexisOauth2ServerApplicationTests {
+	@Test
+	public void contextLoads() {
+	}
+}
