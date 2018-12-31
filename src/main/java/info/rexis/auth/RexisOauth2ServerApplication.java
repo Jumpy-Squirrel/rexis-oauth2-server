284d1f9a683f50cefc899146a825bc39b95cb013
Jumpy-Squirrel <jsquirrel_github_9a6d@packetloss.de>
2018-12-31 11:58:08 +0100

    introduce checkstyle

--- src/main/java/info/rexis/auth/RexisOauth2ServerApplication.java
+++ src/main/java/info/rexis/auth/RexisOauth2ServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RexisOauth2ServerApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(RexisOauth2ServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RexisOauth2ServerApplication.class, args);
+    }
 }
--- src/test/java/info/rexis/auth/RexisOauth2ServerApplicationTests.java
+++ src/test/java/info/rexis/auth/RexisOauth2ServerApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RexisOauth2ServerApplicationTests {
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 }
